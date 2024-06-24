a7786edfcc07e7ec4411fb1291c1879e9a402241
mati-e <matias.espinosa@cloudx.com>
2024-06-24 02:32:58 -0300

    termine POM y Desafio 1

--- cypress/pageObjects/onlineShopPage.js
+++ cypress/pageObjects/onlineShopPage.js
@@ -1,11 +1,111 @@
 class OnlineShop {
-
-    selectors()
-    {
-        
+    //----------------- ELEMENTS -----------------//
+    getOnlineShopLink() {
+        return cy.getByDataCy('onlineshoplink');
     }
 
-    addProduct(name, price, imgUrl, id)
+    getAddProduct() {
+        return cy.getByDataCy('add-product');
+    }
+
+    getProductName() {
+        return cy.getByDataCy('productName');
+    }
+
+    getProductPrice() {
+        return cy.getByDataCy('productPrice');
+    }
+
+    getProductCard() {
+        return cy.getByDataCy('productCard');
+    }
+
+    getProductId() {
+        return cy.getByDataCy('productID');
+    }
+
+    getCreateProduct() {
+        return cy.getByDataCy('createProduct');
+    }
+
+    getCloseModal() {
+        return cy.getByDataCy('closeModal');
+    }
+
+    getSearchType() {
+        return cy.getByDataCy('search-type');
+    }
+
+    getSearchBar() {
+        return cy.getByDataCy('search-bar');
+    }
+
+    getDeleteIcon() {
+        return cy.getByDataCy('delete-666');
+    }
+
+    getSaveDelete() {
+        return cy.get('#saveEdit');
+    }
+
+    getSearchedId() {
+        return cy.getByDataCy('name');
+    }
+
+
+    //----------------- ACTIONS -----------------//
+    goToOnlineShop() {
+        this.getOnlineShopLink().click();
+    }
+
+    openAddProduct() {
+        this.getAddProduct().click();
+    }
+
+    setProductName(productName) {
+        this.getProductName().type(productName);
+    }
+
+    setProductPrice(productPrice) {
+        this.getProductPrice().type(productPrice);
+    }
+
+    setProductCard(productCard) {
+        this.getProductCard().type(productCard);
+    }
+
+    setProductId(productId) {
+        this.getProductId().type(productId);
+    }
+
+    submitAddProduct() {
+        this.getCreateProduct().click();
+    }
+
+    closeModal() {
+        this.getCloseModal().click()
+    }
+
+
+    searchByID() {
+        this.getSearchType().select('id')
+    }
+
+    enterID(productId) {
+        this.getSearchBar().clear()
+        this.getSearchBar().type(`${productId}{enter}`)
+    }
+
+    selectDeleteProduct() {
+        this.getDeleteIcon().click();
+    }
+
+    submitDeleteProduct() {
+        this.getSaveDelete().click();
+    }
+
+    //----------------- COMBINED ACTIONS -----------------//
+    addProductXXXX(name, price, imgUrl, id)
     {
         cy.getByDataCy('onlineshoplink').click();
         cy.getByDataCy('add-product').click();
@@ -15,6 +115,28 @@ class OnlineShop {
         cy.getByDataCy('productID').type(id);
         cy.getByDataCy('createProduct').click();
     }
+
+    addProduct(productName, productPrice, productCard, productId)
+    {
+        this.goToOnlineShop()
+        this.openAddProduct()
+        this.setProductName(productName)
+        this.setProductPrice(productPrice)
+        this.setProductCard(productCard)
+        this.setProductId(productId)
+        this.submitAddProduct()
+    }
+
+    searchProduct(productId) {
+        this.searchByID()
+        this.enterID(productId)
+    }
+
+    deleteProduct() {
+        this.selectDeleteProduct()
+        this.submitDeleteProduct()
+    }
+
 }
 
 export default OnlineShop;

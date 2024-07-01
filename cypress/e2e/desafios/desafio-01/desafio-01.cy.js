39cb4e26ee6672a70f688528cbe4a9787b474eab
mati-e <matias.espinosa@cloudx.com>
2024-06-30 23:30:08 -0300

    code cleaned

--- cypress/e2e/desafios/desafio-01/desafio-01.cy.js
+++ cypress/e2e/desafios/desafio-01/desafio-01.cy.js
@@ -8,56 +8,6 @@ const suiteId = directoryName.split(/[-]/).pop();
 
 
 describe(`${module}`, () => {
-    //IGNORE: First test, no command for data-cy
-    it(`${suiteName}-${suiteId}`, () => {
-        cy.fixture(`${module}/${suiteName}-${suiteId}/${suiteId}.json`).then(data => {
-            cy.log(data.user.name)
-            cy.log(data.user.password)
-        cy.visit('/')
-        cy.get('[data-cy="registertoggle"]').dblclick()
-        cy.get('[data-cy="user"]').type(data.user.name)
-        cy.get('[data-cy="pass"]').type(data.user.password)
-        cy.get('[data-cy="submitForm"]').click()
-        cy.get('[data-cy="onlineshoplink"]').click()
-        cy.get('[data-cy="add-product"]').click()
-        cy.get('[data-cy="productName"]').type(data.producto.name)
-        cy.get('[data-cy="productPrice"]').type(data.producto.price)
-        cy.get('[data-cy="productCard"]').type(data.producto.imgUrl)
-        cy.get('[data-cy="productID"]').type(data.producto.id)
-        cy.get('[data-cy="createProduct"]').click()
-        cy.get('[data-cy="closeModal"]').click()
-        cy.get('[data-cy="search-type"]').select('id')
-        cy.get('[data-cy="search-bar"]').type(`${data.producto.id}{enter}`)
-        cy.get('[data-cy="delete-666"]').click()
-        cy.get('#saveEdit').click()
-        cy.get('[data-cy="closeModal"]').click()
-        })
-    })
-    //IGNORE: Second test, no POM
-    it(`${suiteName}-${suiteId}`, () => {
-        cy.fixture(`${module}/${suiteName}-${suiteId}/${suiteId}.json`).then(data => {
-            cy.log(data.user.name)
-            cy.log(data.user.password)
-        cy.visit('/')
-        cy.getByDataCy('registertoggle').dblclick();
-        cy.getByDataCy('user').type(data.user.name);
-        cy.getByDataCy('pass').type(data.user.password);
-        cy.getByDataCy('submitForm').click();
-        cy.getByDataCy('onlineshoplink').click();
-        cy.getByDataCy('add-product').click();
-        cy.getByDataCy('productName').type(data.producto.name);
-        cy.getByDataCy('productPrice').type(data.producto.price);
-        cy.getByDataCy('productCard').type(data.producto.imgUrl);
-        cy.getByDataCy('productID').type(data.producto.id);
-        cy.getByDataCy('createProduct').click();
-        cy.getByDataCy('closeModal').click();
-        cy.getByDataCy('search-type').select('id');
-        cy.getByDataCy('search-bar').type(`${data.producto.id}{enter}`);
-        cy.getByDataCy('delete-666').click();
-        cy.get('#saveEdit').click();
-        cy.getByDataCy('closeModal').click();
-        })
-    });
     //DESAFIO 1
     it.only(`${suiteName}-${suiteId}`, () => {
         const login=new Login();
@@ -67,21 +17,20 @@ describe(`${module}`, () => {
         cy.visit('/')
         login.loginSuccess(data.user.name,data.user.password)
         cy.url().should('include', '/home')
+        shop.getOnlineShopLink().click();
         shop.addProduct(data.producto.name, data.producto.price, data.producto.imgUrl, data.producto.id)
         cy.intercept('GET', 'https://pushing-it.onrender.com/api/products?page=1&limit=8').as('productsRequest');
         cy.wait('@productsRequest');
         cy.get('#chakra-modal--body-\\:r58\\:')
           .should('have.text', `${data.producto.name} has been added`)
-          .should('exist');
-        shop.closeModal()
+        shop.getCloseModal().click()
         shop.searchProduct(data.producto.id)
         shop.deleteProduct()
         cy.intercept('GET', 'https://pushing-it.onrender.com/api/products?page=1&limit=8').as('productsRequest');
         cy.wait('@productsRequest');
         cy.get('#chakra-modal--body-\\:r58\\:')
           .should('have.text', `${data.producto.name} has been deleted`)
-          .should('exist');
-        shop.closeModal()
+        shop.getCloseModal().click()
         shop.searchProduct(data.producto.id)
         shop.getSearchedId()
             .should('not.exist')
--- cypress/pageObjects/onlineShopPage.js
+++ cypress/pageObjects/onlineShopPage.js
@@ -52,89 +52,27 @@ class OnlineShop {
         return cy.getByDataCy('name');
     }
 
-
     //----------------- ACTIONS -----------------//
-    goToOnlineShop() {
-        this.getOnlineShopLink().click();
-    }
 
-    openAddProduct() {
+    addProduct(productName, productPrice, productCard, productId)
+    {
         this.getAddProduct().click();
-    }
-
-    setProductName(productName) {
-        this.getProductName().type(productName);
-    }
-
-    setProductPrice(productPrice) {
+        this.getProductName().type(productName)
         this.getProductPrice().type(productPrice);
-    }
-
-    setProductCard(productCard) {
         this.getProductCard().type(productCard);
-    }
-
-    setProductId(productId) {
         this.getProductId().type(productId);
-    }
-
-    submitAddProduct() {
         this.getCreateProduct().click();
     }
 
-    closeModal() {
-        this.getCloseModal().click()
-    }
-
-
-    searchByID() {
+    searchProduct(productId) {
         this.getSearchType().select('id')
-    }
-
-    enterID(productId) {
         this.getSearchBar().clear()
         this.getSearchBar().type(`${productId}{enter}`)
     }
 
-    selectDeleteProduct() {
-        this.getDeleteIcon().click();
-    }
-
-    submitDeleteProduct() {
-        this.getSaveDelete().click();
-    }
-
-    //----------------- COMBINED ACTIONS -----------------//
-    addProductXXXX(name, price, imgUrl, id)
-    {
-        cy.getByDataCy('onlineshoplink').click();
-        cy.getByDataCy('add-product').click();
-        cy.getByDataCy('productName').type(name);
-        cy.getByDataCy('productPrice').type(price);
-        cy.getByDataCy('productCard').type(imgUrl);
-        cy.getByDataCy('productID').type(id);
-        cy.getByDataCy('createProduct').click();
-    }
-
-    addProduct(productName, productPrice, productCard, productId)
-    {
-        this.goToOnlineShop()
-        this.openAddProduct()
-        this.setProductName(productName)
-        this.setProductPrice(productPrice)
-        this.setProductCard(productCard)
-        this.setProductId(productId)
-        this.submitAddProduct()
-    }
-
-    searchProduct(productId) {
-        this.searchByID()
-        this.enterID(productId)
-    }
-
     deleteProduct() {
-        this.selectDeleteProduct()
-        this.submitDeleteProduct()
+        this.getDeleteIcon().click();
+        this.getSaveDelete().click();
     }
 
 }
